1b3c47ee98c0615711c72f682d767fde57ee8fd1
balitsky_igor <balitsky_igor@list.ru>
2018-12-22 14:28:01 +0300

    Обертка для контроллера

--- InfrastructureStarter/src/main/java/ru/vood/infrastructure/wrappers/kt/WrapperForControllerKt.kt
+++ InfrastructureStarter/src/main/java/ru/vood/infrastructure/wrappers/kt/WrapperForControllerKt.kt
@@ -77,134 +77,106 @@ interface WrapperForController : BeginnerOfChainFunctionInterface {
         return try {
             val errorMessage = validationFunction.apply(t1)
             if (isNotValid(errorMessage))
-                return validError(errorMessage)
-            val apply = longListFunction.apply(t1)
-            getOk(apply)
+                return validError<R>(errorMessage)
+            getOk<R>(longListFunction.apply(t1))
         } catch (e: Exception) {
-            getError(e)
+            getError<R>(e)
         }
     }
 
     fun <T1, T2, R> validateAndWrapObject(validationFunction: BiFunction<T1, T2, ErrorMessage>, longListFunction: BiFunction<T1, T2, WrapperForService.WrappedObject<R>>, t1: T1, t2: T2): WrappedObject<R> {
-        try {
+        return try {
             val errorMessage = validationFunction.apply(t1, t2)
             if (isNotValid(errorMessage))
-                return validError(errorMessage)
-            val apply = longListFunction.apply(t1, t2)
-            return getOk(apply)
+                return validError<R>(errorMessage)
+            getOk<R>(longListFunction.apply(t1, t2))
         } catch (e: Exception) {
-            return getError(e)
+            getError<R>(e)
         }
-
     }
 
     fun <T1, T2, T3, R> validateAndWrapObject(validationFunction: Function3<T1, T2, T3, ErrorMessage>, longListFunction: Function3<T1, T2, T3, WrapperForService.WrappedObject<R>>, t1: T1, t2: T2, t3: T3): WrappedObject<R> {
-        try {
+        return try {
             val errorMessage = validationFunction.apply(t1, t2, t3)
             if (isNotValid(errorMessage))
-                return validError(errorMessage)
-            val apply = longListFunction.apply(t1, t2, t3)
-            return getOk(apply)
+                return validError<R>(errorMessage)
+            getOk<R>(longListFunction.apply(t1, t2, t3))
         } catch (e: Exception) {
-            return getError(e)
+            getError<R>(e)
         }
-
     }
 
     fun <T1, T2, T3, T4, R> validateAndWrapObject(validationFunction: Function4<T1, T2, T3, T4, ErrorMessage>, longListFunction: Function4<T1, T2, T3, T4, WrapperForService.WrappedObject<R>>, t1: T1, t2: T2, t3: T3, t4: T4): WrappedObject<R> {
-        try {
+        return try {
             val errorMessage = validationFunction.apply(t1, t2, t3, t4)
             if (isNotValid(errorMessage))
-                return validError(errorMessage)
-            val apply = longListFunction.apply(t1, t2, t3, t4)
-            return getOk(apply)
+                return validError<R>(errorMessage)
+            getOk<R>(longListFunction.apply(t1, t2, t3, t4))
         } catch (e: Exception) {
-            return getError(e)
+            getError<R>(e)
         }
-
     }
 
     fun <T1, T2, T3, T4, T5, R> validateAndWrapObject(validationFunction: Function5<T1, T2, T3, T4, T5, ErrorMessage>, longListFunction: Function5<T1, T2, T3, T4, T5, WrapperForService.WrappedObject<R>>, t1: T1, t2: T2, t3: T3, t4: T4, t5: T5): WrappedObject<R> {
-        try {
+        return try {
             val errorMessage = validationFunction.apply(t1, t2, t3, t4, t5)
             if (isNotValid(errorMessage))
-                return validError(errorMessage)
-            val apply = longListFunction.apply(t1, t2, t3, t4, t5)
-            return getOk(apply)
+                return validError<R>(errorMessage)
+            getOk<R>(longListFunction.apply(t1, t2, t3, t4, t5))
         } catch (e: Exception) {
-            return getError(e)
+            getError<R>(e)
         }
-
     }
 
     fun <T1, T2, T3, T4, T5, T6, R> validateAndWrapObject(validationFunction: Function6<T1, T2, T3, T4, T5, T6, ErrorMessage>, longListFunction: Function6<T1, T2, T3, T4, T5, T6, WrapperForService.WrappedObject<R>>, t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6): WrappedObject<R> {
-        try {
+        return try {
             val errorMessage = validationFunction.apply(t1, t2, t3, t4, t5, t6)
             if (isNotValid(errorMessage))
-                return validError(errorMessage)
-            val apply = longListFunction.apply(t1, t2, t3, t4, t5, t6)
-            return getOk(apply)
+                return validError<R>(errorMessage)
+            getOk<R>(longListFunction.apply(t1, t2, t3, t4, t5, t6))
         } catch (e: Exception) {
-            return getError(e)
+            getError<R>(e)
         }
-
     }
 
     fun <T1, T2, T3, T4, T5, T6, T7, R> validateAndWrapObject(validationFunction: Function7<T1, T2, T3, T4, T5, T6, T7, ErrorMessage>, longListFunction: Function7<T1, T2, T3, T4, T5, T6, T7, WrapperForService.WrappedObject<R>>, t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7): WrappedObject<R> {
-        try {
+        return try {
             val errorMessage = validationFunction.apply(t1, t2, t3, t4, t5, t6, t7)
             if (isNotValid(errorMessage))
-                return validError(errorMessage)
-            val apply = longListFunction.apply(t1, t2, t3, t4, t5, t6, t7)
-            return getOk(apply)
+                return validError<R>(errorMessage)
+            getOk<R>(longListFunction.apply(t1, t2, t3, t4, t5, t6, t7))
         } catch (e: Exception) {
-            return getError(e)
+            getError<R>(e)
         }
-
     }
 
     fun <T1, T2, T3, T4, T5, T6, T7, T8, R> validateAndWrapObject(validationFunction: Function8<T1, T2, T3, T4, T5, T6, T7, T8, ErrorMessage>, longListFunction: Function8<T1, T2, T3, T4, T5, T6, T7, T8, WrapperForService.WrappedObject<R>>, t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8): WrappedObject<R> {
-        try {
+        return try {
             val errorMessage = validationFunction.apply(t1, t2, t3, t4, t5, t6, t7, t8)
             if (isNotValid(errorMessage))
-                return validError(errorMessage)
-            val apply = longListFunction.apply(t1, t2, t3, t4, t5, t6, t7, t8)
-            return getOk(apply)
+                return validError<R>(errorMessage)
+            getOk<R>(longListFunction.apply(t1, t2, t3, t4, t5, t6, t7, t8))
         } catch (e: Exception) {
-            return getError(e)
+            getError<R>(e)
         }
-
     }
 
-    ///////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    fun isNotValid(errorMessage: ErrorMessage?): Boolean {
-        return errorMessage !=
-                null && errorMessage.errorMessages != null && errorMessage.errorMessages.size > 0
-    }
+    fun isNotValid(errorMessage: ErrorMessage?) = errorMessage != null && errorMessage.errorMessages != null && errorMessage.errorMessages.size > 0
 
-    fun <R> validError(errorMessage: ErrorMessage): WrappedObject<R> {
-        return WrappedObject<R>(CommonStatus.ERROR, null, errorMessage, null)
-    }
+    fun <R> validError(errorMessage: ErrorMessage) = WrappedObject<R>(CommonStatus.ERROR, null, errorMessage, null)
 
+    fun <R> getError(e: Exception) = WrappedObject<R>(CommonStatus.ERROR, null, ErrorMessage(listOf<String>(e.message!!)), null)
 
-    fun <T> getError(e: Exception): WrappedObject<T> {
-        val errorMessage = ErrorMessage(listOf<String>(e.message!!))
-        return WrappedObject(CommonStatus.ERROR, null, errorMessage, null)
-    }
+    fun <R> getOk(objectFromService: WrapperForService.WrappedObject<R>) =
+            WrappedObject<R>(CommonStatus.OK,
+                    RestContext(Date(),
+                            checkPage(objectFromService.page, objectFromService.objectList)),
+                    null,
+                    objectFromService.objectList)
 
-    fun <R> getOk(objectFromService: WrapperForService.WrappedObject<R>): WrappedObject<R> {
-        return WrappedObject<R>(CommonStatus.OK,
-                RestContext(Date(),
-                        checkPage(objectFromService.page,
-                                objectFromService.objectList)), null,
-                objectFromService.objectList)
-    }
-
-    fun checkPage(page: Page, rs: List<*>): Page {
-        return if (page === Page.NULL_PAGE) Page(rs.size) else page
-    }
-
+    fun checkPage(page: Page, rs: List<*>) = if (page === Page.NULL_PAGE) Page(rs.size) else page
 
     class WrappedObject<OBJ> {
         var status: CommonStatus? = null
